5737dd98af35c29f56c0776b226b90a68420639f
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-10-14 08:00:52 +0900

    wip: http test

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,37 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import express from "express";
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
+
+const app = express();
+app.use(express.json());
 
 AppDataSource.initialize().then(async () => {
+    app.post("/users", async (req, res) => {
+        const user = new User();
+        user.firstName = req.body.firstName;
+        user.lastName = req.body.lastName;
+        user.age = req.body.age;
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+        try {
+            await AppDataSource.manager.save(user);
+            res.status(201).json(user);
+        } catch (error) {
+            res.status(500).json({ message: "Error saving user", error });
+        }
+    });
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+    app.get("/users", async (req, res) => {
+        try {
+            const users = await AppDataSource.manager.find(User);
+            res.json(users);
+        } catch (error) {
+            res.status(500).json({ message: "Error loading users", error });
+        }
+    });
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    app.listen(3000, () => {
+        console.log("Server started on port 3000");
+    });
 
-}).catch(error => console.log(error))
+}).catch(error => console.log(error));
