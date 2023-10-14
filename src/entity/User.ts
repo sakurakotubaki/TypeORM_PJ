b97906b43a9d1d4de6f7a2e968bc1276343d0b21
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-10-14 10:00:24 +0900

    feat: jest mock function

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -2,6 +2,13 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 // MySQLで使用するテーブル名を定義
 @Entity()
 export class User {
+    getName(): any {
+        throw new Error("Method not implemented.")
+    }
+
+    postName(): any {
+        throw new Error("Method not implemented.")
+    }
 
     @PrimaryGeneratedColumn()
     id: number
